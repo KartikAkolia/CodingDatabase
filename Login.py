79610f34b223116a0c370e73904bf9a067ed3003
KartikAkolia <kartikakolia@outlook.com>
2021-11-25 21:17:20 +0000

    commit

--- Login.py
+++ Login.py
@@ -8,13 +8,19 @@ while True:
 
     if u==username[0] and password[0]:
         print("Hello There " + u)
+    else:
+        ctr=ctr+1
     if u==username[1] and password[1]:
-        print("Hello There " + u)
+        print("Hello There " + u)        
+    else:
+        ctr=ctr+1
     if u==username[2] and password[2]:
         print("Hello There " + u)
-    if u!=username[0] + username[1] + username[2] and password[0] + password[1] + password[2]:
-        print("Please Enter either username or password again, First Entry Used, the Maximum Attempts is three")
+    else:
         ctr=ctr+1
+    #if (u!=username[0] or u!=username[1] or u!=username[2]) and (p!=password[0] or p!=password[1] or p!=password[2]):
+    #    print("Please Enter either username or password again, First Entry Used, the Maximum Attempts is three")
+    #    ctr=ctr+1
     if ctr>3:
         print("Maximum Entries Entered Please try again later")
         exit()
