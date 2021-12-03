ba4476e301529a164004dde2605043ab8cd92756
KartikAkolia <94763117+KartikAkolia@users.noreply.github.com>
2021-12-03 20:35:38 +0000

    Add files via upload

---
+++ Updated_Login.py
@@ -0,0 +1,26 @@
+import numpy
+names=numpy.array(["Kartik","Ashley","Oliver"])
+names1=numpy.array([""])
+password=numpy.array(["KAR","ASH","OLI"])
+password1=numpy.array([""])
+
+
+username=input("Enter your Username")
+for n in names:
+    if n!=username:
+        names1=numpy.append(names,username)
+        print("You have been added to the list")
+        break
+    else:
+        break
+
+passw=input("Enter your Password")
+for p in password:
+    if p!=passw:
+        password1=numpy.append(password,passw)
+        print("New Password Added")
+        break
+    else:
+        break
+print(names1)
+print(password1)
