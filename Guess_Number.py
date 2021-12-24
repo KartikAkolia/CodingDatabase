b216dad7bb1415c9d975c83fcb932d00fabf0974
KartikAkolia <94763117+KartikAkolia@users.noreply.github.com>
2021-12-24 22:04:49 +0000

    commit

--- Guess_Number.py
+++ Guess_Number.py
@@ -1,35 +1,40 @@
-class Guess_Number:
+class GuessNumber:
+    def __init__(self):
+        pass
+
     import random
+
     def x(self):
-        ctr=0
+        ctr = 0
 
         while ctr <= 5:
             i = int(input("Enter A Number"))
-            if i <0:
+            if i < 0:
                 print("Positive Numbers Only")
                 continue
             j = self.random.randint(10, 20)
             diff = j - i
-            if diff < 0 or diff >0:
+            if diff < 0 or diff > 0:
                 print("Entered number close to lucky one \n Please try again...")
                 print("The random generated is " + str(j))
-                ctr=ctr+1
+                ctr = ctr + 1
             if diff == 0:
                 print("Well Done You have guessed the number right")
                 print("The random generated is " + str(j))
-                break 
-            #if 0 < diff < 10:
+                break
+                # if 0 < diff < 10:
             #    print("You have almost guessed right number")
             #    print("The random generated is " + str(j))
             #    ctr=ctr+1
-            #if diff!=0:
+            # if diff!=0:
             #    print("Incorrect Guess.Try Again")
             #    print(diff)
             #    ctr=ctr+1
-        if ctr==6:
+        if ctr == 6:
             print("You couldn't guess the number \n Good Luck Next Time ")
 
         print("END")
-    
-n=Guess_Number()
+
+
+n = GuessNumber()
 n.x()
