5939d3db7b9a0d240ad6f1e4137d3e419c0c8305
KartikAkolia <94763117+KartikAkolia@users.noreply.github.com>
2022-03-17 19:27:32 +0000

    commit

--- Game.py
+++ Game.py
@@ -1,28 +1,22 @@
+#!/usr/bin/env python3
 import random
-score=0
-username=["Admin","Kartik","Person","Guest"]
-password=["admin","KAR14","person","guest"]
-u=input("Enter Your Username")
-p=input("Enter Your Password")
-def checking():
-    if u!=username[0]:
-        if u!=username[1]:
-            if u!=username[2]:
-                if u!=username[3]:
-                    print("Wrong Username Entered")
-    if p!=password[0]:
-        if p!=password[1]:
-            if p!=password[2]:
-                if p!=password[3]:
-                    print("Wrong Password Entered")
-checking()
-def stage1():
-    x=random.randint(0,6)
-    value=int(input("Enter A Number"))
-    if value > x:
-        score=score+1
-        print( u + " entered value is " + str(value) + " The random value was " + str(x) + " The score is " + str(score))
-    if value < x:
-        score=score-1
-        print( u + " entered value is " + str(value) + " The random value was " + str(x) + " The score is " + str(score))
-stage1()
+value=[]
+score=""
+username=["Kartik","Neeraj"]
+password=["KARIK","NERAJ".upper]
+x=input("Enter Your Username")
+y=input("Enter Your Password")
+if x!=username[0]:
+    print("Wrong Username Entered")
+if x==username[1]:
+    print("Correct Username Entered")
+if y!=password[0]:
+    print("Wrong Password Entered")
+if y==password[1]:
+    print("Correct Password Entered")
+first_x=random.randint(0,6)
+second_y=random.randint(0,6)
+first_input_x=input("Enter A Digit between 0-6?")
+if first_input_x < first_x:
+    score=score+1
+    print("Well Done " + str(username) + str(score))
