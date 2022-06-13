d69fdf7443a578451e336b8ab451058b46f7bce9
KartikAkolia <94763117+KartikAkolia@users.noreply.github.com>
2022-06-13 15:47:56 +0100

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 import os
 import subprocess
-import _mysql_connector
 
-l=[]
-source=""
-dest=""
+l = []
+source = ""
+dest = ""
 
 directory = os.fsencode("/dev/pts")
+
 
 def bash():
     os.system("clear")
@@ -17,13 +17,16 @@ def bash():
         if ch.upper() == "EXIT":
             break
 
+
 def run_command(cmd):
     os.system(cmd)
+
 
 def send_msg(msg, d):
     cmd = "echo " + msg + ">" + d
     print(cmd)
     os.system(cmd)
+
 
 def chat():
     for file in os.listdir(directory):
@@ -39,16 +42,18 @@ def chat():
     input("Press Key to send message")
     send_msg(msg, dest)
 
+
 def init():
     for file in os.listdir(directory):
         filename = os.fsdecode(file)
         l.append(filename)
     l.sort()
     print(l)
-    ctr =len(l) - 2
-    source="/dev/pts/" + str(l[ctr])
-    dest="/dev/pts/" + str(l[ctr-1])
-    print(source,dest)
+    ctr = len(l) - 2
+    source = "/dev/pts/" + str(l[ctr])
+    dest = "/dev/pts/" + str(l[ctr - 1])
+    print(source, dest)
+
 
 def search_string(ch, filename):
     lines = []
@@ -68,7 +73,7 @@ def search_string(ch, filename):
         y = lines[i].split(":")
         i = len(y) - 1
         while i > 0:
-            result = y[i] + ' ' + result
+            result = y[i] + " " + result
             i = i - 1
         return result
     else:
@@ -77,7 +82,7 @@ def search_string(ch, filename):
 
 
 print("Welcome To Python3_ChatBot")
-#init()
+# init()
 while True:
     q = input("$")
     s = search_string(q, "answers.txt")
@@ -97,21 +102,3 @@ while True:
         chat()
     else:
         print(s)
-
-
-# def check_process():
-#    mydb = mysql.connector.connect(
-#                host="192.168.0.102",
-#                user="kartik",
-#                password="Kartik84@",
-#                database="KARTIK",
-#                port=3306
-#            )
-#    cursor = mydb.cursor()
-#    #machine_id = sp.getoutput("ps auxwww | grep -i 'python3 messaging_service.py' | awk '{print $7}' | head -1")
-#    machine_id = sp.getoutput("tty")
-#    get_ip=sp.getoutput("ifconfig -a | grep -i inet | head -1 | awk '{print $2}'")
-#    system = sp.getoutput("ps auxwww | grep -i 'python3 messaging_service.py' | awk '{print $12}' | head -1")
-#    cursor.execute("INSERT INTO system_id VALUES('" + machine_id + "','" + system + "','" + get_ip + "', 'Y')")
-#    mydb.commit()
-#    cursor.close()
