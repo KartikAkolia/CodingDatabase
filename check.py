cb4f95ac830fecfbef34b5d1dbe862fa7bffd443
KartikAkolia <94763117+KartikAkolia@users.noreply.github.com>
2022-09-05 07:02:11 +0100

    Update check.py

--- check.py
+++ check.py
@@ -18,41 +18,45 @@ while True:
     print("13 /etc/init.d/ssh reload")
     print("14 /etc/init.d/apache2 reload")
     print("15 /etc/init.d/mysql reload")
-    print("16 sudo apt update -y && sudo apt full-upgrade -y && sudo apt autoremove -y && sudo apt clean -y && sudo apt autoclean -y")
+    print(
+        "16 sudo apt update -y && sudo apt full-upgrade -y && sudo apt autoremove -y && sudo apt clean -y && sudo apt autoclean -y"
+    )
     print("17 exit")
     ch = "0"
-    ch=input("Enter Your Choice")
+    ch = input("Enter Your Choice")
     if ch == "1":
         os.system("1 /etc/init.d/ssh status")
     if ch == "2":
-        os.system("/etc/init.d/apache2 status") 
+        os.system("/etc/init.d/apache2 status")
     if ch == "3":
         os.system("/etc/init.d/mysql status")
     if ch == "4":
-        os.system("/etc/init.d/ssh start") 
+        os.system("/etc/init.d/ssh start")
     if ch == "5":
-        os.system("/etc/init.d/apache2 start") 
+        os.system("/etc/init.d/apache2 start")
     if ch == "6":
-        os.system("/etc/init.d/mysql start") 
+        os.system("/etc/init.d/mysql start")
     if ch == "7":
-        os.system("/etc/init.d/ssh stop") 
+        os.system("/etc/init.d/ssh stop")
     if ch == "8":
-        os.system("/etc/init.d/apache2 stop") 
+        os.system("/etc/init.d/apache2 stop")
     if ch == "9":
-        os.system("/etc/init.d/mysql stop") 
+        os.system("/etc/init.d/mysql stop")
     if ch == "10":
-        os.system("/etc/init.d/ssh restart") 
+        os.system("/etc/init.d/ssh restart")
     if ch == "11":
-        os.system("/etc/init.d/apache2 restart") 
+        os.system("/etc/init.d/apache2 restart")
     if ch == "12":
-        os.system("/etc/init.d/mysql restart") 
+        os.system("/etc/init.d/mysql restart")
     if ch == "13":
         os.system("/etc/init.d/ssh reload")
     if ch == "14":
-        os.system("/etc/init.d/apache2 reload") 
+        os.system("/etc/init.d/apache2 reload")
     if ch == "15":
         os.system("/etc/init.d/mysql reload")
     if ch == "16":
-        os.system("sudo apt update -y && sudo apt full-upgrade -y && sudo apt autoremove -y && sudo apt clean -y && sudo apt autoclean -y")
-    if ch=="17":
+        os.system(
+            "sudo apt update -y && sudo apt full-upgrade -y && sudo apt autoremove -y && sudo apt clean -y && sudo apt autoclean -y"
+        )
+    if ch == "17":
         exit()
